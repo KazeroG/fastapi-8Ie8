cb7b9e42cd1daa8aad5abbb77d9e8ee6e884e5d4
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-05-14 02:50:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from weasyprint import HTML
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+
+class HTMLRequest(BaseModel):
+    html: str
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post('/convert')
+async def convert_to_pdf(request: Request, html_request: HTMLRequest):
+    html_content = html_request.html
+    pdf_bytes = HTML(string=html_content).write_pdf()
+    return pdf_bytes
